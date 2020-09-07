2e30b71f1ad5bae0789a44780ecb8ac60daa25dd
703669046 <703669046@qq.com>
2020-09-07 20:46:21 +0800

    客户资金发票预开接口对接、预算执行bug处理

---
+++ src/api/fund/management/index.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+
+//获取表格数据
+export function getListTable(params) {
+  return request({
+    url:'/fund/advance/list',
+    method:'get',
+    params
+  })
+}
+//获取状态列表
+export function getStateList() {
+  return request({
+    url:'/dict/budget/project-status',
+    method:'get'
+  })
+}
+//新增记录
+export function addDate(data){
+  return request({
+    url:'/fund/advance/add',
+    method:'post',
+    data
+  })
+}
+//办理业务下拉
+export function getBusiness(){
+  return request({
+    url:'/dict/fund/invoice-business',
+    method:'get'
+  })
+}
+
+
