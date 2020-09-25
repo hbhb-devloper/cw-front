3bab8f04921a53d58118082144ddfe4c51e612a7
703669046 <703669046@qq.com>
2020-09-25 19:20:20 +0800

    日常性费用接口对接 [ci skip]

---
+++ src/api/budget/daily/index.js
@@ -0,0 +1,35 @@
+import request from '@/utils/request'
+
+//查询单位
+export function getCompany(){
+  return request({
+    url:'/unit/tree-select',
+    method:'get',
+  })
+}
+export function getDataList(params) {
+  return request({
+    url:'/budget/agile/list',
+    method:'get',
+    params
+  })
+}
+export function getTypeList() {
+  return request({
+    url:'/dict/fund/budget-type',
+    method:'get'
+  })
+}
+export function addDate(data) {
+  return request({
+    url:'/budget/agile',
+    method:'post',
+    data
+  })
+}
+export function getInfoDate(id) {
+  return request({
+    url:`/budget/agile/info/${id}`,
+    method:'get'
+  })
+}
