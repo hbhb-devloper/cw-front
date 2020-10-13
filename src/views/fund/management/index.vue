278642ba6a6b53da218a9f289812008f6e7ba853
703669046 <703669046@qq.com>
2020-10-13 11:42:34 +0800

    部署

--- src/views/fund/management/index.vue
+++ src/views/fund/management/index.vue
@@ -223,6 +223,7 @@
       </el-table-column>
 
 
+
       <el-table-column prop="itemName" align="center" width="150" label="操作">
         <template slot-scope="scope">
           <el-button :class="scope.row.isCancellation?'red':''"
