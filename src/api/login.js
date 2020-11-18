eb239ef853c6c90ec43d2ea3f9ad300814f97fd9
cangyezhi <1019095884@qq.com>
2020-11-18 18:57:37 +0800

    合并测试服[ci skip]

--- src/api/login.js
+++ src/api/login.js
@@ -1,23 +1,18 @@
-import request from '@/utils/request1'
-import request1 from '@/utils/request'
+import request from '@/utils/request'
 import {Encrypt} from '@/utils/AESCrypt'
-const client_id = 'client-app'
+const client_id = 'web'
 const client_secret = '123456'
 const grant_type = 'password'
 const scope = 'server'
 
 // 登录方法
-export function check(data) {
-    let info ={
-        captcha:data.captcha,
-        captchaKey:data.captchaKey
-    }
-  return request({
-    url: '/auth/captcha/check',
-    method: 'get',
-    params: info
-  })
-}
+// export function login(username, password, code, uuid) {
+//   return request({
+//     url: '/auth/oauth/token',
+//     method: 'post',
+//     params: { username, password, code, uuid, client_id, client_secret, grant_type, scope }
+//   })
+// }
 
 // 登录方法
 export function login(data) {
@@ -37,24 +32,18 @@ export function login(data) {
         return obj;
     }
     let userInfo=deepClone(data)
-    // userInfo.password=Encrypt(userInfo.password)
-    userInfo.grant_type =grant_type
-    userInfo.client_id =client_id
-    userInfo.client_secret  =client_secret
-    userInfo.captcha  =undefined
-    userInfo.captchaKey  =undefined
-    userInfo.rememberMe  =undefined
+    userInfo.password=Encrypt(userInfo.password)
     return request({
-        url: '/auth/oauth/token',
+        url: '/login',
         method: 'post',
-        params: userInfo
+        data: userInfo
     })
 }
 
 
 // 获取用户详细信息
 export function getInfo() {
-    return request1({
+    return request({
         url: '/getInfo',
         method: 'get'
     })
@@ -63,15 +52,95 @@ export function getInfo() {
 // 退出方法
 export function logout() {
     return request({
-        url: '/auth/oauth/logout',
-        method: 'DELETE'
+        url: '/logout',
+        method: 'get'
     })
 }
 
 // 获取验证码
 export function getCodeImg() {
     return request({
-        url: '/auth/captcha',
+        url: '/captcha',
         method: 'get'
     })
 }
+
+
+
+// import request from '@/utils/request1'
+// import request1 from '@/utils/request'
+// import {Encrypt} from '@/utils/AESCrypt'
+// const client_id = 'client-app'
+// const client_secret = '123456'
+// const grant_type = 'password'
+// const scope = 'server'
+
+// // 登录方法
+// export function check(data) {
+//     let info ={
+//         captcha:data.captcha,
+//         captchaKey:data.captchaKey
+//     }
+//   return request({
+//     url: '/auth/captcha/check',
+//     method: 'get',
+//     params: info
+//   })
+// }
+
+// // 登录方法
+// export function login(data) {
+//     function deepClone(obj) {
+//         let result = typeof obj.splice === "function" ? [] : {};
+//         if (obj && typeof obj === 'object') {
+//             for (let key in obj) {
+//                 if (obj[key] && typeof obj[key] === 'object') {
+//                     result[key] = deepClone(obj[key]); //如果对象的属性值为object的时候，递归调用deepClone,即在吧某个值对象复制一份到新的对象的对应值中。
+//                 } else {
+//                     result[key] = obj[key]; //如果对象的属性值不为object的时候，直接复制参数对象的每一个键值到新的对象对应的键值对中。
+//                 }
+    
+//             }
+//             return result;
+//         }
+//         return obj;
+//     }
+//     let userInfo=deepClone(data)
+//     // userInfo.password=Encrypt(userInfo.password)
+//     userInfo.grant_type =grant_type
+//     userInfo.client_id =client_id
+//     userInfo.client_secret  =client_secret
+//     userInfo.captcha  =undefined
+//     userInfo.captchaKey  =undefined
+//     userInfo.rememberMe  =undefined
+//     return request({
+//         url: '/auth/oauth/token',
+//         method: 'post',
+//         params: userInfo
+//     })
+// }
+
+
+// // 获取用户详细信息
+// export function getInfo() {
+//     return request1({
+//         url: '/getInfo',
+//         method: 'get'
+//     })
+// }
+
+// // 退出方法
+// export function logout() {
+//     return request({
+//         url: '/auth/oauth/logout',
+//         method: 'DELETE'
+//     })
+// }
+
+// // 获取验证码
+// export function getCodeImg() {
+//     return request({
+//         url: '/auth/captcha',
+//         method: 'get'
+//     })
+// }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,14 @@
-import { login, logout, getInfo, check } from '@/api/login'
+import { login, logout, getInfo } from '@/api/login'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import Layout from '@/layout/index'
-// import { filterAsyncRouter  } from '@/utils/Routerfilter'
-import { asyncRouter } from '@/router/asyncRouter.js'
+import {Encrypt} from '@/utils/AESCrypt'
+
 const user = {
     state: {
         token: getToken(),
         name: '',
         avatar: '',
-        nickname: '',
+        nickname:'',
         roles: [],
         permissions: []
     },
@@ -20,8 +20,8 @@ const user = {
         SET_NAME: (state, name) => {
             state.name = name
         },
-        SET_NICKNEM: (state, nickname) => {
-            state.nickname = nickname;
+        SET_NICKNEM:(state,nickname)=>{
+            state.nickname=nickname;
         },
         SET_AVATAR: (state, avatar) => {
             state.avatar = avatar
@@ -38,16 +38,11 @@ const user = {
         // 登录
         Login({ commit }, userInfo) {
             return new Promise((resolve, reject) => {
-                check(userInfo).then(res => {
-                    login(userInfo).then(res => {
-                        setToken('Bearer ' + res.accessToken)
-                        commit('SET_TOKEN', 'Bearer ' + res.accessToken)
-                        resolve()
-                    }).catch(error => {
-                        reject(error)
-                    })
-                }
-                ).catch(error => {
+                login(userInfo).then(res => {
+                    setToken(res)
+                    commit('SET_TOKEN', res)
+                    resolve()
+                }).catch(error => {
                     reject(error)
                 })
             })
@@ -56,24 +51,20 @@ const user = {
         // 获取用户信息
         GetInfo({ commit, state }) {
             return new Promise((resolve, reject) => {
-                getInfo(state.token).then(res => {
-                    commit('SET_NAME', res.userInfo.userName)
-                    commit('SET_AVATAR', res.userInfo.avatar)
-                    commit('SET_NICKNEM', res.userInfo.nickName)
-                    commit('SET_PERMISSIONS', res.permissions)
-                    let namefilter = ['角色库', '角色分配', '工作台', '流程类型', '流程列表']
-                    const sideRouters = filterAsyncRouter(res.sideRouters)
-                    // const sideRouters1 = filterAsyncRouter1(asyncRouter, namefilter)
-                    // console.log('sideRouters1', sideRouters1);
-                    // const sideRouters = filterAsyncRouter(sideRouters1)
-                    // const navRouters = filterAsyncRouter(res.navRouters)
-                    commit('SET_SIDEROUTERS', sideRouters)
-                    // commit('SET_NAVROUTERS', navRouters)
-                    resolve(sideRouters)
-                }).catch(error => {
-                    reject(error)
-                })
-            })
+                  getInfo(state.token).then(res => {
+                      commit('SET_NAME', res.userInfo.userName)
+                      commit('SET_AVATAR', res.userInfo.avatar)
+                      commit('SET_NICKNEM',res.userInfo.nickName)
+                      commit('SET_PERMISSIONS', res.permissions)
+                      const sideRouters = filterAsyncRouter(res.sideRouters)
+                      const navRouters = filterAsyncRouter(res.navRouters)
+                      commit('SET_SIDEROUTERS', sideRouters)
+                      commit('SET_NAVROUTERS', navRouters)
+                      resolve(sideRouters)
+                  }).catch(error => {
+                      reject(error)
+                  })
+              })
         },
 
 
@@ -104,73 +95,6 @@ const user = {
 
 
 }
-function getarr(b, a) {
-    return b.reduce((k, m) => {
-
-        let obj = {
-            name: m.name,
-            path: m.path,
-            component: m.component,
-            meta: m.meta,
-            hidden: m.hidden,
-            redirect: m.redirect,
-            children: []
-        }
-        if (m.children) {
-            for (let j of m.children) {
-                if (j.children) {
-                    getarr(m.children, a, k)
-                } else {
-                    if (a.includes(j.meta.title)) {
-                        obj.children.push(j)
-                    }
-                }
-            }
-        } else {
-            if (a.includes(m.meta.title)) {
-                obj.children.push(m)
-            }
-        }
-        if (obj.children.length) {
-            k.push(obj)
-        }
-
-        return k
-    }, [])
-}
-/**
- * 通过meta.role判断是否与当前用户权限匹配
- * @param namefilter
- * @param route
- */
-function hasPermission(namefilter, route) {
-    if (route.perms) {
-        return namefilter.some(role => route.perms.includes(role))
-    } else {
-        return true
-    }
-}
-/**
- * 递归过滤异步路由表，返回符合用户角色权限的路由表
- * @param routes asyncRouterMap
- * @param namefilter
- */
-function filterAsyncRouter1(routes, namefilter) {
-    // console.log('namefilter', namefilter);
-    // console.log('routes', routes);
-    const res = []
-    routes.forEach(route => {
-        const tmp = { ...route }
-        if (hasPermission(namefilter, tmp)) {
-            if (tmp.children) {
-                tmp.children = filterAsyncRouter1(tmp.children, namefilter)
-            }
-            res.push(tmp)
-        }
-    })
-
-    return res
-}
 
 // 遍历后台传来的路由字符串，转换为组件对象
 function filterAsyncRouter(asyncRouterMap) {
@@ -179,18 +103,12 @@ function filterAsyncRouter(asyncRouterMap) {
             // Layout组件特殊处理
             if (route.component === 'Layout') {
                 route.component = Layout
-            }
-            else {
+            } else {
                 route.component = loadView(route.component)
             }
         }
         if (route.children != null && route.children && route.children.length) {
             route.children = filterAsyncRouter(route.children)
-        }
-        if (route.children) {
-            if (route.children.length == 0) {
-                route.hidden = true
-            }
         }
         return true
     })
@@ -199,3 +117,206 @@ export const loadView = (view) => { // 路由懒加载
     return (resolve) => require([`@/views/${view}`], resolve)
 }
 export default user
+
+
+// import { login, logout, getInfo, check } from '@/api/login'
+// import { getToken, setToken, removeToken } from '@/utils/auth'
+// import Layout from '@/layout/index'
+// // import { filterAsyncRouter  } from '@/utils/Routerfilter'
+// import { asyncRouter } from '@/router/asyncRouter.js'
+// const user = {
+//     state: {
+//         token: getToken(),
+//         name: '',
+//         avatar: '',
+//         nickname: '',
+//         roles: [],
+//         permissions: []
+//     },
+
+//     mutations: {
+//         SET_TOKEN: (state, token) => {
+//             state.token = token
+//         },
+//         SET_NAME: (state, name) => {
+//             state.name = name
+//         },
+//         SET_NICKNEM: (state, nickname) => {
+//             state.nickname = nickname;
+//         },
+//         SET_AVATAR: (state, avatar) => {
+//             state.avatar = avatar
+//         },
+//         SET_ROLES: (state, roles) => {
+//             state.roles = roles
+//         },
+//         SET_PERMISSIONS: (state, permissions) => {
+//             state.permissions = permissions
+//         }
+//     },
+
+//     actions: {
+//         // 登录
+//         Login({ commit }, userInfo) {
+//             return new Promise((resolve, reject) => {
+//                 check(userInfo).then(res => {
+//                     login(userInfo).then(res => {
+//                         setToken('Bearer ' + res.accessToken)
+//                         commit('SET_TOKEN', 'Bearer ' + res.accessToken)
+//                         resolve()
+//                     }).catch(error => {
+//                         reject(error)
+//                     })
+//                 }
+//                 ).catch(error => {
+//                     reject(error)
+//                 })
+//             })
+//         },
+
+//         // 获取用户信息
+//         GetInfo({ commit, state }) {
+//             return new Promise((resolve, reject) => {
+//                 getInfo(state.token).then(res => {
+//                     commit('SET_NAME', res.userInfo.userName)
+//                     commit('SET_AVATAR', res.userInfo.avatar)
+//                     commit('SET_NICKNEM', res.userInfo.nickName)
+//                     commit('SET_PERMISSIONS', res.permissions)
+//                     let namefilter = ['角色库', '角色分配', '工作台', '流程类型', '流程列表']
+//                     const sideRouters = filterAsyncRouter(res.sideRouters)
+//                     // const sideRouters1 = filterAsyncRouter1(asyncRouter, namefilter)
+//                     // console.log('sideRouters1', sideRouters1);
+//                     // const sideRouters = filterAsyncRouter(sideRouters1)
+//                     // const navRouters = filterAsyncRouter(res.navRouters)
+//                     commit('SET_SIDEROUTERS', sideRouters)
+//                     // commit('SET_NAVROUTERS', navRouters)
+//                     resolve(sideRouters)
+//                 }).catch(error => {
+//                     reject(error)
+//                 })
+//             })
+//         },
+
+
+//         // 退出系统
+//         LogOut({ commit, state }) {
+//             return new Promise((resolve, reject) => {
+//                 logout(state.token).then(() => {
+//                     commit('SET_TOKEN', '')
+//                     commit('SET_ROLES', [])
+//                     commit('SET_PERMISSIONS', [])
+//                     removeToken()
+//                     resolve()
+//                 }).catch(error => {
+//                     reject(error)
+//                 })
+//             })
+//         },
+
+//         // 前端 登出
+//         FedLogOut({ commit }) {
+//             return new Promise(resolve => {
+//                 commit('SET_TOKEN', '')
+//                 removeToken()
+//                 resolve()
+//             })
+//         }
+//     },
+
+
+// }
+// function getarr(b, a) {
+//     return b.reduce((k, m) => {
+
+//         let obj = {
+//             name: m.name,
+//             path: m.path,
+//             component: m.component,
+//             meta: m.meta,
+//             hidden: m.hidden,
+//             redirect: m.redirect,
+//             children: []
+//         }
+//         if (m.children) {
+//             for (let j of m.children) {
+//                 if (j.children) {
+//                     getarr(m.children, a, k)
+//                 } else {
+//                     if (a.includes(j.meta.title)) {
+//                         obj.children.push(j)
+//                     }
+//                 }
+//             }
+//         } else {
+//             if (a.includes(m.meta.title)) {
+//                 obj.children.push(m)
+//             }
+//         }
+//         if (obj.children.length) {
+//             k.push(obj)
+//         }
+
+//         return k
+//     }, [])
+// }
+// /**
+//  * 通过meta.role判断是否与当前用户权限匹配
+//  * @param namefilter
+//  * @param route
+//  */
+// function hasPermission(namefilter, route) {
+//     if (route.perms) {
+//         return namefilter.some(role => route.perms.includes(role))
+//     } else {
+//         return true
+//     }
+// }
+// /**
+//  * 递归过滤异步路由表，返回符合用户角色权限的路由表
+//  * @param routes asyncRouterMap
+//  * @param namefilter
+//  */
+// function filterAsyncRouter1(routes, namefilter) {
+//     // console.log('namefilter', namefilter);
+//     // console.log('routes', routes);
+//     const res = []
+//     routes.forEach(route => {
+//         const tmp = { ...route }
+//         if (hasPermission(namefilter, tmp)) {
+//             if (tmp.children) {
+//                 tmp.children = filterAsyncRouter1(tmp.children, namefilter)
+//             }
+//             res.push(tmp)
+//         }
+//     })
+
+//     return res
+// }
+
+// // 遍历后台传来的路由字符串，转换为组件对象
+// function filterAsyncRouter(asyncRouterMap) {
+//     return asyncRouterMap.filter(route => {
+//         if (route.component) {
+//             // Layout组件特殊处理
+//             if (route.component === 'Layout') {
+//                 route.component = Layout
+//             }
+//             else {
+//                 route.component = loadView(route.component)
+//             }
+//         }
+//         if (route.children != null && route.children && route.children.length) {
+//             route.children = filterAsyncRouter(route.children)
+//         }
+//         if (route.children) {
+//             if (route.children.length == 0) {
+//                 route.hidden = true
+//             }
+//         }
+//         return true
+//     })
+// }
+// export const loadView = (view) => { // 路由懒加载
+//     return (resolve) => require([`@/views/${view}`], resolve)
+// }
+// export default user
