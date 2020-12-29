ee0a600e0faefeea970d5b7c3418dd06a815b703
wangxiaogang <15151269@163.com>
2020-12-29 21:26:29 +0800

    修改AES加密配置

--- src/utils/AESCrypt.js
+++ src/utils/AESCrypt.js
@@ -13,7 +13,7 @@ export function Encrypt(word) {
   const encrypted = CryptoJS.AES.encrypt(srcs, KEY, {
     iv: IV,
     mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.Pkcs7,
+    padding: CryptoJS.pad.ZeroPadding,
   });
   return CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
 }
