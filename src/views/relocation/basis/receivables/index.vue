16e4fe2f3dec13572678e84bad77c11ed8bce374
cangyezhihi_admin <1019095884@qq.com>
2021-01-11 00:16:04 +0800

    修改文案

--- src/views/relocation/basis/receivables/index.vue
+++ src/views/relocation/basis/receivables/index.vue
@@ -168,7 +168,7 @@
             type="text"
             icon="el-icon-edit"
             @click="handleAdd(scope.row)"
-            >添加</el-button
+            >收款</el-button
           >
         </template>
       </el-table-column>
