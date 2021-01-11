c53d60a3ec8e40850d836fb9dc879e1c0446489e
cangyezhihi_admin <1019095884@qq.com>
2021-01-11 22:13:09 +0800

    收据修改权限

--- src/views/relocation/basis/receipt/index.vue
+++ src/views/relocation/basis/receipt/index.vue
@@ -194,6 +194,7 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
+            v-hasPermi="['relocation:receipt:updata']"
             >修改</el-button
           >
           <el-button
